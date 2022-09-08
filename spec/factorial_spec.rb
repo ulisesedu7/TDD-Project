045a37cc5458b2eb64f313f4f69f524bc43aeb6d
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-08 11:13:00 -0600

    Create tests for the fizzbuzz exercise

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -22,4 +22,13 @@ describe Solver do
       expect(@solver.reverse('Hamburguer')).to eq('reugrubmaH')
     end
   end
+
+  describe 'FizzBuzz' do
+    it 'The fizzbuzz solver should return a string with the word fizz, buzz or fizzbuzz depending on the number' do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(@solver.fizzbuzz(8)).to eq('8')
+    end
+  end
 end
